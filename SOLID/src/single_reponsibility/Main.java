5635eb1bdc310409c0cbeee498803f4b58dc2844
Rashed <rashedulislam8741@yahoo.com>
2024-03-01 17:15:13 +0600

    added single responsibility

---
+++ SOLID/src/single_reponsibility/Main.java
@@ -0,0 +1,25 @@
+package single_reponsibility;
+
+public class Main {
+    public static void main(String[] args) {
+        TextManipulate textManipulate = new TextManipulate("");
+        TextPrint textPrint = new TextPrint(textManipulate);
+
+        textManipulate.appendText("Hello!");
+        System.out.println("append: " + textPrint.printText());
+        textManipulate.appendText("How are you?");
+        System.out.println("append: " + textPrint.printText());
+        textManipulate.textFind("are");
+        System.out.println("find: " + textPrint.printText());
+        textManipulate.appendText("Hope you are doings well.");
+        System.out.println("append: " + textPrint.printText());
+        textManipulate.textFindAndReplace("doings", "doing as");
+        System.out.println("findReplace: " + textPrint.printText());
+        textManipulate.textFindAndDelete("as");
+        System.out.println("findDelete: " + textPrint.printText());
+
+        System.out.println("============================");
+        Printer printer = new Printer(textManipulate);
+        printer.printer();
+    }
+}
